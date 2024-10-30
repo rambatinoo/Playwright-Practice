6e23fd54b26c37753ef1d26b5bce739f2bc88dae
rambatinoo <barrywjames1990@gmail.com>
2024-10-30 14:05:52 +0000

    added test for invalid youtube login

---
+++ Youtube/youtubeLogin.js
@@ -0,0 +1,38 @@
+const { chromium } = require("playwright");
+
+async function youtubeInvalidLogin() {
+  // launch browser
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  // go to Hacker News
+  await page.goto("https://youtube.com");
+
+  // accept initial cookies
+  await page.locator('button:has-text("Accept all")').click();
+
+  await page.locator('a:has-text("Sign in")').first().click();
+
+  await page.waitForSelector("#identifierId");
+
+  await page.fill("#identifierId", "invalidID@bubbles.com");
+
+  await page.click('button:has-text("Next")');
+
+  await page.waitForTimeout(1000);
+
+  if (
+    await page
+      .locator('.Ekjuhf.Jj6Lae:has-text("Couldn\'t find your Google Account")')
+      .isVisible()
+  ) {
+    console.log("Error message correctly displayed.");
+  } else {
+    console.log("Test Failed");
+  }
+
+  await browser.close();
+}
+
+youtubeInvalidLogin();
